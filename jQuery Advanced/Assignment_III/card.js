45123c9f49b47ed967d4814b7ad2f1ee4049c4e0
AJ May <ajmay@microsoft.com>
2016-09-28 12:32:16 -0700

    Set up files for assignment 3

---
+++ jQuery Advanced/Assignment_III/card.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  // $('#addClass').click(function(){});
+  //alert('hooked up and works');
+  $('#add_button').click(function(){
+    var row = "<tr><td>" + $('#fn').val() + "</td><td>" + $('#ln').val() + "</td><td>" + $('#em').val() + "</td><td>" + $('#con').val() + "</td></tr>";
+    //console.log(row);
+    // $('#table_users').append(row);
+    // $('.user_input').val("");
+    return false;
+  });
+});
