39bc05f96f3dbd69fcb59b6c78267f63b13db2e6
AJ May <ajmay@microsoft.com>
2016-10-02 10:58:18 -0700

    finished assignment six.

--- jQuery Advanced/Assignment_VI/date_validations.js
+++ jQuery Advanced/Assignment_VI/date_validations.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   // $('#addClass').click(function(){});
   //alert('hooked up and works');
+  // $('#name_alert').hide();
   $( ".trip_date" ).datepicker();
   $( "#confirmation" ).dialog({
     autoOpen: false,
@@ -15,17 +16,21 @@ $(document).ready(function(){
   });
 
   $('#check_dates').click(function(){
-    console.log($('#to').val());
-    console.log($('#from').val());
-    console.log($('#name').val());
+    $('#name_alert').hide();
+    // display error message if user doesn't enter name
+    if (!$('#name').val()) {
+      console.log('Name is blank');
+      $('#name_alert').show();
+      return false;
+    }
+    //Create dialog displaying trip details
     var msg = "Thank you, " + $('#name').val() + ". Your trip leaves on " + $('#from').val() + " and returns on " + $('#to').val() + "."
     console.log(msg);
     $("#confirmation").append("<p>"+msg+"</p>")
     $("#confirmation").dialog( "open" );
-
-    if ($('#name').val()) {
-      console.log('Name is blank');
-    }
+    //clear input fields
+    $('#to, #from, #name').val("");
+    //cancel the form submission
     return false;
   });
 });
