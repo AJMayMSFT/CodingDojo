0c6aa25f80515a506365b45bf00ada8372c95b92
AJ May <ajmay@microsoft.com>
2016-11-08 21:07:33 -0800

    Integrating models exercise

--- rails/integrating_models/app/controllers/users_controller.rb
+++ rails/integrating_models/app/controllers/users_controller.rb
@@ -5,4 +5,19 @@ class UsersController < ApplicationController
 
   def new
   end
+  def create
+    @user = User.create(name: params[:name])
+    render json: @user
+  end
+  def show
+    #render text: 'show method'
+    render json: User.find(params[:id])
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def total
+    # render text: 'total method'
+    render text: "There are #{User.all.count} records."
+  end
 end
--- rails/integrating_models/config/routes.rb
+++ rails/integrating_models/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root 'users#index'
   get 'users/index'
 
   get 'users/new'
+  get 'users/total' => 'users#total'
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
