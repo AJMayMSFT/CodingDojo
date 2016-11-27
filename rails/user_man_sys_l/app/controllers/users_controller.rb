d996a573e59d5096c60243c60c8f13be54088157
AJ May <ajmay@live.com>
2016-11-27 10:35:48 -0800

    finished user management III

--- rails/user_man_sys_l/app/controllers/users_controller.rb
+++ rails/user_man_sys_l/app/controllers/users_controller.rb
@@ -16,6 +16,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
+    redirect_to '/users'
+  end
+
+  def destroy
+    User.find(params[:id]).delete
+    redirect_to '/users'
+  end
+
   private
   def user_params
    params.require(:user).permit(:first_name, :last_name, :email_address, :password)
--- rails/user_man_sys_l/config/routes.rb
+++ rails/user_man_sys_l/config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   post '/users' => 'users#create'
   get '/users' => 'users#index'
   get '/users/:id' => 'users#show'
+  patch '/users/:id' => 'users#update'
+  delete '/users/:id' => 'users#destroy'
+  get '/users/:id/edit' => 'users#edit'
+  # post '/users/update' => 'users#update'
   # get 'users/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
