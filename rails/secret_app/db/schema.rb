b82a39840ca88955df91472f2efee52270021d1e
AJ May <ajmay@live.com>
2016-12-14 14:21:27 -0800

    updated secrets_app to postgresql

--- rails/secret_app/db/schema.rb
+++ rails/secret_app/db/schema.rb
@@ -12,13 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20161208031321) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "likes", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "secret_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["secret_id"], name: "index_likes_on_secret_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
+    t.index ["secret_id"], name: "index_likes_on_secret_id", using: :btree
+    t.index ["user_id"], name: "index_likes_on_user_id", using: :btree
   end
 
   create_table "secrets", force: :cascade do |t|
@@ -26,7 +29,7 @@ ActiveRecord::Schema.define(version: 20161208031321) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_secrets_on_user_id"
+    t.index ["user_id"], name: "index_secrets_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +40,7 @@ ActiveRecord::Schema.define(version: 20161208031321) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "likes", "secrets"
+  add_foreign_key "likes", "users"
+  add_foreign_key "secrets", "users"
 end
